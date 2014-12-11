f9a37bbe1f8e9053a4ebed23ae3dbc8619ec427e
RedArmy1 <330982851@qq.com>
2014-12-11 23:12:36 +0800

    test/InitServletTest.java

---
+++ ExtStudy/src/com/ext/test/InitServletTest.java
@@ -0,0 +1,22 @@
+package com.ext.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by qwn on 2014/12/11.
+ */
+public class InitServletTest extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("test web");
+    }
+}
